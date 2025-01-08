3fcedf9439e7ad3fceca02c6741103b1e233e51e
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-01-08 10:49:06 -0300

    feat: add connection to mongodb

---
+++ db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+// ConnMongo connects to MongoDB
+func ConnMongo() (*mongo.Client, error) {
+	// connection string
+	clientOpions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// getting username and password from environment variables
+	username := os.Getenv("MONGO_DB_USERNAME")
+	password := os.Getenv("MONGO_DB_PASSWORD")
+
+	// settimg auth
+	clientOpions.SetAuth(options.Credential{
+		Username: username,
+		Password: password,
+	})
+
+	// connect to MongoDB
+	client, err := mongo.Connect(context.Background(), clientOpions)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	log.Println("Connected to MongoDB!")
+
+	return client, nil
+}
+
+// GetCollection returns the collection
+func GetCollection() *mongo.Collection {
+	return collection
+}
