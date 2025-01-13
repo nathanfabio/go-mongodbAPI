2c846792e69b3f0c81944b4279f9d41971d37528
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-01-13 10:20:49 -0300

    feat setting up main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/nathanfabio/go-mongodbAPI/db"
+	"github.com/nathanfabio/go-mongodbAPI/handlers"
+	"github.com/nathanfabio/go-mongodbAPI/services"
+)
+
+type Application struct {
+	Models services.Models
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	mongoClient, err := db.ConnMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15 * time.Second)
+	defer cancel()
+
+	defer func ()  {
+		if err = mongoClient.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	services.NewClient(mongoClient)
+
+	log.Panicln("Running in port 8080")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CreateRouter()))
 }
