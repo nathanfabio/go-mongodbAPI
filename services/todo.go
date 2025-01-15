1462aafce58a8dd7eef3349da734d9c46c74cd0f
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-01-15 13:52:25 -0300

    feat: add InsertTodo function to handle todo insertion

--- services/todo.go
+++ services/todo.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"context"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -21,4 +23,26 @@ func NewClient(mongo *mongo.Client) Todo {
 	client = mongo
 
 	return Todo{}
+}
+
+func returnCollection(collection string) *mongo.Collection {
+	return client.Database("mongo_api").Collection(collection)
+}
+
+func (t *Todo) InsertTodo(input Todo) error {
+	collection := returnCollection("todos")
+
+	_, err := collection.InsertOne(context.TODO(), Todo{
+		Task:      input.Task,
+		Completed: input.Completed,
+		CreatedAt: time.Now(),
+		UpdateAt:  time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error on insert todo: ", err)
+		return err
+	}
+
+	return nil
 }
